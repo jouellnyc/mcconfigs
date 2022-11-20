9fcad100781b299b967ec6d698fb17e09fc87985
JohnO <jouellnyc@gmail.com>
2022-11-20 10:41:26 -0500

    reorg

--- hardware/sh1106.py
+++ hardware/sh1106.py
@@ -1,15 +1,11 @@
-
-# https://cdn.hackaday.io/files/1654057076687520/sh1106.py
-
-
-
 #
 # MicroPython SH1106 OLED driver, I2C and SPI interfaces
 #
 # The MIT License (MIT)
 #
 # Copyright (c) 2016 Radomir Dopieralski (@deshipu),
-#               2017 Robert Hammelrath (@robert-hh)
+#               2017-2021 Robert Hammelrath (@robert-hh)
+#               2021 Tim Weber (@scy)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +25,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #
-# Sample code sections
+# Sample code sections for ESP8266 pin assignments
 # ------------ SPI ------------------
 # Pin Map SPI
 #   - 3v - xxxxxx   - Vcc
@@ -91,51 +87,59 @@ _HIGH_COLUMN_ADDRESS = const(0x10)
 _SET_PAGE_ADDRESS    = const(0xB0)
 
 
-class SH1106:
-    def __init__(self, width, height, external_vcc):
+class SH1106(framebuf.FrameBuffer):
+
+    def __init__(self, width, height, external_vcc, rotate=0):
         self.width = width
         self.height = height
         self.external_vcc = external_vcc
+        self.flip_en = rotate == 180 or rotate == 270
+        self.rotate90 = rotate == 90 or rotate == 270
         self.pages = self.height // 8
-        self.buffer = bytearray(self.pages * self.width)
-        fb = framebuf.FrameBuffer(self.buffer, self.width, self.height,
-                                  framebuf.MVLSB)
-        self.framebuf = fb
-# set shortcuts for the methods of framebuf
-        self.fill = fb.fill
-        self.fill_rect = fb.fill_rect
-        self.hline = fb.hline
-        self.vline = fb.vline
-        self.line = fb.line
-        self.rect = fb.rect
-        self.pixel = fb.pixel
-        self.scroll = fb.scroll
-        self.text = fb.text
-        self.blit = fb.blit
+        self.bufsize = self.pages * self.width
+        self.renderbuf = bytearray(self.bufsize)
+        self.pages_to_update = 0
 
+        if self.rotate90:
+            self.displaybuf = bytearray(self.bufsize)
+            # HMSB is required to keep the bit order in the render buffer
+            # compatible with byte-for-byte remapping to the display buffer,
+            # which is in VLSB. Else we'd have to copy bit-by-bit!
+            super().__init__(self.renderbuf, self.height, self.width,
+                             framebuf.MONO_HMSB)
+        else:
+            self.displaybuf = self.renderbuf
+            super().__init__(self.renderbuf, self.width, self.height,
+                             framebuf.MONO_VLSB)
+
+        # flip() was called rotate() once, provide backwards compatibility.
+        self.rotate = self.flip
         self.init_display()
 
     def init_display(self):
         self.reset()
         self.fill(0)
         self.poweron()
-        self.show()
+        # rotate90 requires a call to flip() for setting up.
+        self.flip(self.flip_en)
 
     def poweroff(self):
         self.write_cmd(_SET_DISP | 0x00)
 
     def poweron(self):
         self.write_cmd(_SET_DISP | 0x01)
+        time.sleep_ms(100)
 
-    def rotate(self, flag, update=True):
-        if flag:
-            self.write_cmd(_SET_SEG_REMAP | 0x01)  # mirror display vertically
-            self.write_cmd(_SET_SCAN_DIR | 0x08)  # mirror display hor.
-        else:
-            self.write_cmd(_SET_SEG_REMAP | 0x00)
-            self.write_cmd(_SET_SCAN_DIR | 0x00)
+    def flip(self, flag=None, update=True):
+        if flag is None:
+            flag = not self.flip_en
+        mir_v = flag ^ self.rotate90
+        mir_h = flag
+        self.write_cmd(_SET_SEG_REMAP | (0x01 if mir_v else 0x00))
+        self.write_cmd(_SET_SCAN_DIR | (0x08 if mir_h else 0x00))
+        self.flip_en = flag
         if update:
-            self.show()
+            self.show(True) # full update
 
     def sleep(self, value):
         self.write_cmd(_SET_DISP | (not value))
@@ -147,14 +151,82 @@ class SH1106:
     def invert(self, invert):
         self.write_cmd(_SET_NORM_INV | (invert & 1))
 
-    def show(self):
-        for page in range(self.height // 8):
-            self.write_cmd(_SET_PAGE_ADDRESS | page)
-            self.write_cmd(_LOW_COLUMN_ADDRESS | 2)
-            self.write_cmd(_HIGH_COLUMN_ADDRESS | 0)
-            self.write_data(self.buffer[
-                self.width * page:self.width * page + self.width
-            ])
+    def show(self, full_update = False):
+        # self.* lookups in loops take significant time (~4fps).
+        (w, p, db, rb) = (self.width, self.pages,
+                          self.displaybuf, self.renderbuf)
+        if self.rotate90:
+            for i in range(self.bufsize):
+                db[w * (i % p) + (i // p)] = rb[i]
+        if full_update:
+            pages_to_update = (1 << self.pages) - 1
+        else:
+            pages_to_update = self.pages_to_update
+        #print("Updating pages: {:08b}".format(pages_to_update))
+        for page in range(self.pages):
+            if (pages_to_update & (1 << page)):
+                self.write_cmd(_SET_PAGE_ADDRESS | page)
+                self.write_cmd(_LOW_COLUMN_ADDRESS | 2)
+                self.write_cmd(_HIGH_COLUMN_ADDRESS | 0)
+                self.write_data(db[(w*page):(w*page+w)])
+        self.pages_to_update = 0
+
+    def pixel(self, x, y, color=None):
+        if color is None:
+            return super().pixel(x, y)
+        else:
+            super().pixel(x, y , color)
+            page = y // 8
+            self.pages_to_update |= 1 << page
+
+    def text(self, text, x, y, color=1):
+        super().text(text, x, y, color)
+        self.register_updates(y, y+7)
+
+    def line(self, x0, y0, x1, y1, color):
+        super().line(x0, y0, x1, y1, color)
+        self.register_updates(y0, y1)
+
+    def hline(self, x, y, w, color):
+        super().hline(x, y, w, color)
+        self.register_updates(y)
+
+    def vline(self, x, y, h, color):
+        super().vline(x, y, h, color)
+        self.register_updates(y, y+h-1)
+
+    def fill(self, color):
+        super().fill(color)
+        self.pages_to_update = (1 << self.pages) - 1
+
+    def blit(self, fbuf, x, y, key=-1, palette=None):
+        super().blit(fbuf, x, y, key, palette)
+        self.register_updates(y, y+self.height)
+
+    def scroll(self, x, y):
+        # my understanding is that scroll() does a full screen change
+        super().scroll(x, y)
+        self.pages_to_update =  (1 << self.pages) - 1
+
+    def fill_rect(self, x, y, w, h, color):
+        super().fill_rect(x, y, w, h, color)
+        self.register_updates(y, y+h-1)
+
+    def rect(self, x, y, w, h, color):
+        super().rect(x, y, w, h, color)
+        self.register_updates(y, y+h-1)
+
+    def register_updates(self, y0, y1=None):
+        # this function takes the top and optional bottom address of the changes made
+        # and updates the pages_to_change list with any changed pages
+        # that are not yet on the list
+        start_page = y0 // 8
+        end_page = y1 // 8 if y1 is not None else start_page
+        # rearrange start_page and end_page if coordinates were given from bottom to top
+        if start_page > end_page:
+            start_page, end_page = end_page, start_page
+        for page in range(start_page, end_page+1):
+            self.pages_to_update |= 1 << page
 
     def reset(self, res):
         if res is not None:
@@ -168,34 +240,22 @@ class SH1106:
 
 class SH1106_I2C(SH1106):
     def __init__(self, width, height, i2c, res=None, addr=0x3c,
-                 external_vcc=False):
+                 rotate=0, external_vcc=False):
         self.i2c = i2c
         self.addr = addr
         self.res = res
         self.temp = bytearray(2)
-        if hasattr(self.i2c, "start"):
-            self.write_data = self.sw_write_data
-        else:
-            self.write_data = self.hw_write_data
         if res is not None:
             res.init(res.OUT, value=1)
-        super().__init__(width, height, external_vcc)
+        super().__init__(width, height, external_vcc, rotate)
 
     def write_cmd(self, cmd):
         self.temp[0] = 0x80  # Co=1, D/C#=0
         self.temp[1] = cmd
         self.i2c.writeto(self.addr, self.temp)
 
-    def hw_write_data(self, buf):
+    def write_data(self, buf):
         self.i2c.writeto(self.addr, b'\x40'+buf)
-
-    def sw_write_data(self, buf):
-        self.temp[0] = self.addr << 1
-        self.temp[1] = 0x40  # Co=0, D/C#=1
-        self.i2c.start()
-        self.i2c.write(self.temp)
-        self.i2c.write(buf)
-        self.i2c.stop()
 
     def reset(self):
         super().reset(self.res)
@@ -203,7 +263,7 @@ class SH1106_I2C(SH1106):
 
 class SH1106_SPI(SH1106):
     def __init__(self, width, height, spi, dc, res=None, cs=None,
-                 external_vcc=False):
+                 rotate=0, external_vcc=False):
         self.rate = 10 * 1000 * 1000
         dc.init(dc.OUT, value=0)
         if res is not None:
@@ -214,7 +274,7 @@ class SH1106_SPI(SH1106):
         self.dc = dc
         self.res = res
         self.cs = cs
-        super().__init__(width, height, external_vcc)
+        super().__init__(width, height, external_vcc, rotate)
 
     def write_cmd(self, cmd):
         self.spi.init(baudrate=self.rate, polarity=0, phase=0)
