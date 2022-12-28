8a9ece9e67196aa0c91f9f4d4d7fa6b211796448
JohnO <jouellnyc@gmail.com>
2022-12-28 00:50:11 -0500

    +Watch Support

--- hardware/config.py
+++ hardware/config.py
@@ -7,8 +7,8 @@ wires = "left"
 """ What type of Micro Controller? """
 mc = "esp32-devkit-c"
 
-""" Is the Screen 'oled' or 'ili9341' or 'lilygo_ili9341_2_4'? """
-screen = 'lilygo_ili9341_2_4'
+""" Is the Screen 'oled' or 'ili9341' or 'lilygo_ili9341_2_4' or 'lilygo_watch' ? """
+screen = 'lilygo_watch'
 
 """ What are the type of ili9341 supported? """
 ili_screen_types = ['ili9341', 'lilygo_ili9341_2_4']
